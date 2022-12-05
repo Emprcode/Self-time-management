4994ac956be6ba04c22734d8ca57167d08bfb328
Narayan Aryal <aryal.coding@gmail.com>
2022-12-05 11:28:27 +1100

    server is created

--- api/server.js
+++ api/server.js
@@ -1,1 +1,27 @@
-import express  from "express";
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.use("/api/v1/tasks", (req, res, next) => {
+  res.json({
+    message: "sup",
+  });
+});
+
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Hey",
+  });
+});
+app.post("/", (req, res, next) => {
+  res.json({
+    message: "Hey sup",
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`you server running at http://localhost:${PORT} `);
+});
