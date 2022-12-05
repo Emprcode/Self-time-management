03f5a8727b7b44690585f6ea0f6fdb7b3815f12c
Narayan Aryal <aryal.coding@gmail.com>
2022-12-05 13:06:47 +1100

    code added

--- api/server.js
+++ api/server.js
@@ -4,9 +4,29 @@ import taskRouter from "./src/routers/taskRouter.js";
 const app = express();
 const PORT = 8000;
 
-app.use("/api/v1/task", (req, res, next) => {
-  res.json({
-    message: "success",
+app.use("/api/v1/tasks/", taskRouter);
+
+// handeling all uncaught router request
+
+app.use("*", (req, res, next) => {
+  // res.status(400).json({
+  //   status: "error",
+  //   message: "404 not found",
+  // });
+  const error = {
+    code: 404,
+    message: "404 not found",
+  };
+  next(error);
+});
+
+// handling all global error
+
+app.use((error, req, res, next) => {
+  const statusCode = error.code || 500;
+  res.status(statusCode).json({
+    status: "error",
+    message: error.message,
   });
 });
 
