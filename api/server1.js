106976762d141bf11854ce08cff89c94f0964a1b
Narayan Aryal <aryal.coding@gmail.com>
2022-12-08 12:51:48 +1100

    revised code

--- api/server1.js
+++ api/server1.js
@@ -3,6 +3,22 @@ import express from 'express'
 const app = express();
 const PORT = 8000;
 
+app.get("/", (req, res, next) => {
+    res.json({
+        message:"success"
+    })
+})
+app.post("/", (req, res, next) => {
+    res.json({
+        message:"success"
+    })
+})
+app.delete("/", (req, res, next) => {
+    res.json({
+        message:"success"
+    })
+})
+
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`your server running at http://localhost:${PORT}`)
