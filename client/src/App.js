07115ff68aa7ee9afff6cb104adbcdaf288fa87c
Narayan Aryal <aryal.coding@gmail.com>
2022-12-09 00:23:54 +1100

    random test

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import { Title } from "./components/Title";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Form } from "./components/form";
 
 function App() {
   return (
     <div className="App">
       {/* title */}
       <Title />
-      <Form />
     </div>
   );
 }
