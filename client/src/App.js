2fbd0cb5df55634899126f4aeaedd066aa8b5ca4
Narayan Aryal <aryal.coding@gmail.com>
2022-12-09 00:30:18 +1100

    searchbar created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Title } from "./components/Title";
+import { searchBar } from "./components/searchBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       {/* title */}
       <Title />
+      <searchBar />
     </div>
   );
 }
