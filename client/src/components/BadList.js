f540680c50008d4b3f9befdf2299bcf98e8015a1
Narayan Aryal <aryal.coding@gmail.com>
2022-12-31 22:35:50 +1100

    new changes

--- client/src/components/BadList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const BadList = () => {
-  return (
-    <div>
-          <h2> Bad List</h2>
-    </div>
-  )
-}
--- client/src/components/EntryList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const EntryList = () => {
-  return (
-    <div>
-      <h2> Entry List</h2>
-    </div>
-  );
-};
